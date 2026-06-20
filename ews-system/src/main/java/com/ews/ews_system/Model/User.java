7235d898528dd37f22001d48f2d4744c985858d5
Rishabh Singh <singhrishabh4438@gmail.com>
2026-06-20 15:04:08 +0530

    postgres user.java add table property

--- ews-system/src/main/java/com/ews/ews_system/Model/User.java
+++ ews-system/src/main/java/com/ews/ews_system/Model/User.java
@@ -1,13 +1,11 @@
 package com.ews.ews_system.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
